b8b06a391bc7691b7b872ef1ac97faf76215b7d5
BH_Lin <binghuanlin@gmail.com>
2025-08-12 20:14:43 +0800

    Apply safe drawing insets to Scaffold layout
    Updated MainActivity to use WindowInsets.safeDrawing for contentWindowInsets in Scaffold and wrapped ForecastRoute in a Box with padding. This ensures proper handling of system UI insets for edge-to-edge layouts.

--- app/src/main/java/com/bh/weatherforecastandroid/MainActivity.kt
+++ app/src/main/java/com/bh/weatherforecastandroid/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,8 +24,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherForecastAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ForecastRoute(viewModel = ForecastViewModel())
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets.safeDrawing
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        ForecastRoute(viewModel = ForecastViewModel())
+                    }
                 }
             }
         }
